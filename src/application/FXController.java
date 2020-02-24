d5272443ee0bfb91974dd237ae8a7eadde21a93d
cswartz9 <Cswartz9@Gmail.com>
2020-02-23 23:51:58 -0500

    Code for capturing/displaying video -- doesn't work yet

--- src/application/FXController.java
+++ src/application/FXController.java
@@ -1,4 +1,69 @@
 package application;
-public class FXController {
 
+import java.io.ByteArrayInputStream;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+import org.opencv.core.Mat;
+import org.opencv.core.MatOfByte;
+import org.opencv.imgcodecs.Imgcodecs;
+import org.opencv.videoio.VideoCapture;
+
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+public class FXController {
+	@FXML
+	private Button startButton;
+	@FXML
+	private ImageView frameView;
+
+	private ScheduledExecutorService timer;
+	private VideoCapture capture;
+
+	private boolean cameraOn;
+
+
+	public FXController() {
+		cameraOn = false;
+	}
+
+	@FXML
+	void startCamera(ActionEvent event) {
+		if (cameraOn) {
+			cameraOn = false;
+			startButton.setText("Start Camera");
+			timer.shutdown();
+			this.capture.release();
+		}
+		else {
+			capture.open(0);
+			cameraOn = true;
+			Runnable frameGrabber = new Runnable() {
+				public void run() {
+					Mat frame = new Mat();
+					capture.read(frame);
+
+					MatOfByte frameByteMat = new MatOfByte();
+					Imgcodecs.imencode(".bmp", frame, frameByteMat);
+					Image frameImage = new Image(new ByteArrayInputStream(frameByteMat.toArray()));
+
+					Platform.runLater(() -> {
+						frameView.imageProperty().set(frameImage);
+					});
+				}
+			};
+
+			timer = Executors.newSingleThreadScheduledExecutor();
+			timer.scheduleAtFixedRate(frameGrabber, 0, 20, TimeUnit.MILLISECONDS);
+
+			startButton.setText("Stop Camera");
+
+		}
+	}
 }
--- src/application/Main.java
+++ src/application/Main.java
@@ -1,6 +1,4 @@
 package application;
-	
-import java.io.IOException;
 
 import javafx.application.Application;
 import javafx.stage.Stage;
@@ -8,17 +6,27 @@ import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
 import javafx.fxml.FXMLLoader;
 
+import java.io.IOException;
+
+import org.opencv.core.Core;
+
 
 public class Main extends Application {
 	@Override
-	public void start(Stage primaryStage) throws IOException {
+	public void start(Stage mainWindow) throws IOException {
 		FXMLLoader loader = new FXMLLoader(getClass().getResource("vidStreamFX.fxml"));
 		BorderPane root = (BorderPane) loader.load();
 		FXController controller = loader.getController();
+
+		Scene videoScene = new Scene(root, 960, 540);
+		mainWindow.setTitle("OpenCV + JavaFX Video");
+		mainWindow.setScene(videoScene);
+		mainWindow.show();
 	}
 
-	
+
 	public static void main(String[] args) {
+		System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
 		launch(args);
 	}
 }
