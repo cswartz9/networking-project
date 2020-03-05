7bc8433d0b853bec0a538a1755fbb4653e759077
cswartz9 <Cswartz9@Gmail.com>
2020-03-05 18:44:13 -0500

    Comments

--- src/application/FXController.java
+++ src/application/FXController.java
@@ -17,6 +17,10 @@ import javafx.scene.control.Button;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.SocketException;
+
 public class FXController {
 	@FXML
 	private Button startButton;
@@ -35,32 +39,51 @@ public class FXController {
 
 	@FXML
 	void startCamera(ActionEvent event) {
-		if (cameraOn) {
+		if (cameraOn) { // user wants to stop recording
 			cameraOn = false;
 			startButton.setText("Start Camera");
 			timer.shutdown();
 			this.capture.release();
 		}
-		else {
+		else { // user wants to start recording
 			cameraOn = true;
 			capture = new VideoCapture(0);
-			Runnable frameGrabber = new Runnable() {
-				public void run() {
-					Mat frame = new Mat();
-					capture.read(frame);
 
-					MatOfByte frameByteMat = new MatOfByte();
-					Imgcodecs.imencode(".bmp", frame, frameByteMat);
-					Image frameImage = new Image(new ByteArrayInputStream(frameByteMat.toArray()));
+			// create socket for UDP sending
+			try {
+				DatagramSocket socket = new DatagramSocket(1234);
+			} catch (SocketException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+
+			// this executes on a thread to grab frames, display them, send them
+			Runnable frameGrabber = new Runnable() {
+				public void run() { // default method when runnable executes
+					Mat frame = new Mat(); // matrix that holds image information
+					capture.read(frame); // read in from framegrabber to matrix
+
+					MatOfByte frameByteMat = new MatOfByte(); // create specific byte mat for conversions
+					Imgcodecs.imencode(".bmp", frame, frameByteMat); // encode from original matrix to byte matrix as bmp
+					Image frameImage = new Image(new ByteArrayInputStream(frameByteMat.toArray())); // convert byte matrix to Image
 
 					Platform.runLater(() -> {
-						frameView.imageProperty().set(frameImage);
+						try {
+							sendFrame(frameByteMat); // send frame on UDP
+						} catch (SocketException e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						}
+						frameView.imageProperty().set(frameImage); // update current image in frame
 					});
+				}
+				public void sendFrame(MatOfByte frameByteMat) throws SocketException { // TODO
+
 				}
 			};
 
-			timer = Executors.newSingleThreadScheduledExecutor();
-			timer.scheduleAtFixedRate(frameGrabber, 0, 20, TimeUnit.MILLISECONDS);
+			timer = Executors.newSingleThreadScheduledExecutor(); // create new thread for image-capturing
+			timer.scheduleAtFixedRate(frameGrabber, 0, 20, TimeUnit.MILLISECONDS); // run frameGrabber every 20ms
 
 			startButton.setText("Stop Camera");
 
